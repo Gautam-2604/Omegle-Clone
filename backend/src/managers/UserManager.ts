ec380716640312a98c3d1924ea3f2d4cb7be00f5
Gautam-2604 <gaurisariag@gmail.com>
2024-02-19 22:09:07 +0530

    queue creation

--- backend/src/managers/UserManager.ts
+++ backend/src/managers/UserManager.ts
@@ -16,8 +16,16 @@ export class UserManager{
             socket
         })
         this.queue.push(socket.id);
+        this.clearQueue();
     }
-    removeUser(socketId){
+    removeUser(socketId: string){
         this.users=this.users.filter(x=>x.socket.id===socketId)
+        this.queue = this.queue.filter(x => x === socketId);
+    }
+    clearQueue(){
+        if(this.queue.length<2){
+            return;
+        }
+        const user1=this.users.find(x=>x.socket.id===this.queue.pop())
     }
 }
